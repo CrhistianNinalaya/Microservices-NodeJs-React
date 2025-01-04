8b07458b16d5d74a96401d391f1e633e25397c8d
Crhistian Ninalaya <133820344+CrhistianNinalaya@users.noreply.github.com>
2025-01-03 22:43:30 -0500

    Add PostList component to display fetched posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import PostCreate from './Post/PostCreate';
+import PostList from './Post/PostList';
 
 const App = () => {
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h2>Posts</h2>
+      <PostList />
     </div>
   );
 };
