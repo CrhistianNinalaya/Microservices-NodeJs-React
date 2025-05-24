646a7f407fd1c460fc000db8e9ceabc93df4fcfc
Crhistian Ninalaya <133820344+CrhistianNinalaya@users.noreply.github.com>
2025-05-23 21:50:06 -0500

    Refactor comments and posts services to handle events asynchronously and improve error logging

--- comments/index.js
+++ comments/index.js
@@ -2,28 +2,44 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const commentsByPostId = {};
+
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-const commentsByPostId = {};
-
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content: content });
+  comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  });
+
   res.status(201).send(comments);
+});
+
+app.post('/events', async (req, res) => {
+  console.log('Event received:', req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
