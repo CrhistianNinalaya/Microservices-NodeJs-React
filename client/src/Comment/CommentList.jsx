2fd1266788ca08f7adc5dd823d62060050cab2f6
Crhistian Ninalaya <133820344+CrhistianNinalaya@users.noreply.github.com>
2025-05-25 16:06:11 -0500

    Refactor CommentList to display comments based on their status and update PostList to pass comments prop

--- client/src/Comment/CommentList.jsx
+++ client/src/Comment/CommentList.jsx
@@ -1,30 +1,30 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const response = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(response?.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-  });
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    switch (comment.status) {
+      case 'approved':
+      content = comment.content;
+      break;
+      case 'pending':
+      content = 'This comment is awaiting moderation';
+      break;
+      case 'rejected':
+      content = 'This comment has been rejected';
+      break;
+      default:
+      content = '';
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
 };
 
 CommentList.propTypes = {
-  postId: PropTypes.string.isRequired,
+  comments: PropTypes.array.isRequired,
 };
 
 export default CommentList;
